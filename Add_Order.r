c002bc9722a55dcfc4662a7915474e8f69b147db
zandile mdiniso <zandile.mdiniso@dadevs.com>
2023-05-09 08:57:37 +0200

    add order skeleton

---
+++ Add_Order.r
@@ -0,0 +1,77 @@
+#sample dataframe 
+menu <- data.frame(Item = c("Burger", "Pizza", "Fries", "Soda"), Price = c(10, 15, 5, 2)) # nolint
+
+# Define the function to process new orders
+process_order <- function() { # nolint
+
+  # Initialize empty order list
+  order_list <- list()
+   # nolint
+    cat("Food Menu:\n")
+    print(menu)
+
+  repeat {
+    # Print the food menu and prompt the user to select an item
+
+  item <- readline(prompt = "Enter the name of the item you wish to add to your order (or type 'done' to finish): ")   # nolint: line_length_linter.
+
+
+    # Check if the user wants to finish the order
+    if (item == "done") {
+
+      # Print the order summary and prompt the user to confirm the order
+      cat("Order summary:\n")
+      for (i in seq_along(order_list)) {
+        cat(paste0(order_list[[i]]$quantity, "x ", order_list[[i]]$item, ": R", order_list[[i]]$price, "\n"))   # nolint
+      }
+      confirm <- readline(prompt = "Confirm your order? (y/n): ")
+      if (confirm == "y") {
+        # Save the order into the ORDER table (you'll need to define the table first)   # nolint
+        # ...
+        cat("Thank you for your order!\n")
+        break
+      }
+    }
+        quantity <- as.numeric(readline(prompt = "Enter the quantity for this item: "))  # nolint
+    if (is.na(quantity) || quantity <= 0) {
+      cat("Invalid quantity. Please enter a positive integer.\n")
+      next
+    }
+    # nolint
+    # Find the selected item in the menu and calculate the total price
+    item_info <- menu[menu$Item == item, ]
+    if (nrow(item_info) == 0) {
+      cat("Invalid item. Please select an item from the menu.\n")
+      next
+    }
+    price <- item_info$Price * quantity
+     # nolint
+     # nolint
+    # Add the selected item to the order list
+    order_list[[length(order_list) + 1]] <- list(item = item, quantity = quantity, price = price) # nolint
+      # nolint: trailing_whitespace_linter.
+    # Prompt the user to add another item to the order
+    add_another <- readline(prompt = "Do you want to add another item to your order? (y/n): ")  # nolint
+    if (add_another == "n") {
+
+      # Print the order summary and prompt the user to confirm the order
+      cat("Order summary:\n")
+      for (i in seq_along(order_list)) {
+        cat(paste0(order_list[[i]]$quantity, "x ", order_list[[i]]$item, ": R", order_list[[i]]$price, "\n"))  # nolint
+      }
+      confirm <- readline(prompt = "Proceed to checkout? (y/n): ")
+      if (confirm == "y") {
+        # Save the order into the ORDER table (still need to define the table first) # nolint
+        # ...
+        cat("Thank you for your order!\n")
+        break
+      } else {
+        # Start a new order
+        order_list <- list()
+        next
+      }
+    }
+  }
+}
+
+
