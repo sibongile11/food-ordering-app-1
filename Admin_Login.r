f4b44aa00df5cc169b0973708366180717f8eb9b
sibongile11 <sibongile.mokoena@dadevs.co.za>
2023-05-09 15:05:11 +0200

    Admin Login Script

--- Admin_Login.r
+++ Admin_Login.r
@@ -1,1 +1,29 @@
 
+# Load necessary packages
+library(DBI)
+library(RPostgreSQL)
+source("con.R")
+
+
+# Set up database connection
+#con <- dbConnect(RPostgreSQL::PostgreSQL(), dbname = "mydatabase", host = "localhost", user = "myuser", password = "mypassword")
+
+# Prompt user for login credentials
+username <- readline("Enter your username: ")
+password <- readline("Enter your password: ")
+
+# Construct SQL query to check credentials against database
+sql <- paste0("SELECT * FROM Administrator WHERE Admin_user_name = '", username, "' AND Admin_password = '", password, "'")
+
+# Execute query and check number of rows returned
+res <- dbGetQuery(con, sql)
+if (nrow(res) == 1) {
+  # Successful login, grant access to resources
+  print("Login successful!")
+} else {
+  # Failed login, deny access
+  print("Invalid username or password.")
+}
+
+# Close database connection
+#dbDisconnect(con)
